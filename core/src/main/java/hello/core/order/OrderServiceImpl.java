3c7f4f1e12719b3db7d8e886c83480c471251aec
Hyebeen.So <hyebeen0328@naver.com>
2021-12-05 02:04:50 +0900

    싱글톤 컨테이너 - @Configuration과 싱글톤  - MemberRepository 싱글톤 검증 테스트

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -20,4 +20,9 @@ public class OrderServiceImpl implements OrderService{
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
+
+    //for test
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }

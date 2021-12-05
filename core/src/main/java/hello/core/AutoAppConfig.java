36eeb7d0dcc90b7efdb603e30cc98f2c7cfc6f52
Hyebeen.So <hyebeen0328@naver.com>
2021-12-05 20:27:35 +0900

    컴포넌트 스캔 - 탐색 위치와 기본 스캔대상  - @ComponentScan( basePackages, basePackageClasses )  * @Component 를 포함하는 어노테이션들   > @Configuration   > @Controller   > @Service   > @Repository

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan( //@Component Annotation 붙은 모든 클래스를 스프링 빈으로 등록한다.
+//        basePackages = {"hello.core.discount","hello.core.member","hello.core.order"}, //@Component 탐색하는 시작위치 지정. 없으면 컴포넌트 스캔을 위해 '모든' Java 코드를 검색하느라 많은 시간이 소요됨.
+//        basePackageClasses = AutoAppConfig.class, //@ComponentScan 이 선언된 파일의 package hello.core; 참고하여 hello.core 부터 탐색 (default)
+        //권장 : basePackage 를 지정하지 않고, AppConfig 위치를 프로젝트 최상단에 둔다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) //AppConfig 가 컴포넌트 스캔되는 것을 방지하기 위한 필터
 )
 public class AutoAppConfig {
--- core/src/main/java/hello/core/CoreApplication.java
+++ core/src/main/java/hello/core/CoreApplication.java
@@ -3,7 +3,7 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //@ComponentScan 을 포함한다. 즉, 스프링부트를 사용하면 따로 @ComponentScan 을 사용할 필요가 없다.
 public class CoreApplication {
 
 	public static void main(String[] args) {

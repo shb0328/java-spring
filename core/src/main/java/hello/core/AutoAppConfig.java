b89d442bad1fd3e9f566bd096895ae6e6004c954
Hyebeen.So <hyebeen0328@naver.com>
2021-12-05 21:18:44 +0900

    컴포넌트 스캔 - 중복 등록과 충돌
     - 자동 빈 등록 vs. 자동 빈 등록
      > ConflictingBeanDefinitionException
    
     - 수동 빈 등록 vs. 자동 빈 등록
    
      > Spring Test : 오버라이딩
      [main] ... Overriding bean definition for bean 'memoryMemberRepository' with a different definition: replacing [Generic bean: class [hello.core.member.MemoryMemberRepository]; ...
    
      > SpringBoot CoreApplication 실행 : 오류 발생
      The bean 'memoryMemberRepository', defined in class path resource [hello/core/AutoAppConfig.class], could not be registered. A bean with that name has already been defined in file ...
    
      by setting spring.main.allow-bean-definition-overriding=true
    
      * 항상, SpringBoot 처럼 애매한 상황을 만들지 않는 것을 권장

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -13,4 +16,10 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
     //@Bean 클래스가 없다.
+
+    //수동 빈 등록 vs. 자동 빈 등록 충돌 테스트
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }

db75c5948257f75a4a4e3496fcd7a8c2bae8ce60
Hyebeen.So <hyebeen0328@naver.com>
2021-12-05 21:21:14 +0900

    [원복] 컴포넌트 스캔 - 중복 등록과 충돌

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,8 +1,5 @@
 package hello.core;
 
-import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -16,10 +13,4 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
     //@Bean 클래스가 없다.
-
-    //수동 빈 등록 vs. 자동 빈 등록 충돌 테스트
-    @Bean(name = "memoryMemberRepository")
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
 }

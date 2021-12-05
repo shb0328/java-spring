3e06bea226730ede82ccd610aba05437235a8a02
Hyebeen.So <hyebeen0328@naver.com>
2021-12-05 14:40:04 +0900

    싱글톤 컨테이너 - @Configuration과 바이트코드 조작의 마법  ● @Configuration 사용    = CGLIB 기술 사용     * CGLIB : 바이트 조작 라이브러리    - hello.core.AppConfig$$EnhancerBySpringCGLIB     - AppConfig@CGLIB (extends AppConfig)    - call AppConfig.memberRepository 1회 호출     1 memberService -> memberRepository : Spring Bean     2 orderService -> memberRepository : Spring Bean     3 memberRepository : Spring Bean     > 1,2,3 모두 같은 객체
     ● @Configuration 사용X
      = CGLIB 기술 사용X
      - 내가 만든 AppConfig 사용
      - call AppConfig.memberRepository 3회 호출
       1 memberService -> memberRepository : Spring Bean X
       2 orderService -> memberRepository : Spring Bean X
       3 memberRepository : Spring Bean
       > 1,2,3 모두 다른 객체
       > 해결방법은? @Autowired 사용

--- core/src/test/java/hello/core/singleton/configurationSingletonTest.java
+++ core/src/test/java/hello/core/singleton/configurationSingletonTest.java
@@ -29,4 +29,12 @@ public class configurationSingletonTest {
 
         assertThat(memberRepository1).isSameAs(memberRepository2).isSameAs(memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+    }
 }

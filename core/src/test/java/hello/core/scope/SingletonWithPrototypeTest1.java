c48056cbe55813e1b45b80f50f8b1ed359fcf4bd
Hyebeen.So <hyebeen0328@naver.com>
2021-12-28 11:59:25 +0900

    빈 스코프 @Scope
    >프로토타입 스코프를 싱글톤 빈과 함께 사용시 문제점 해결
     ※ DL(Dependency Lookup) : 의존관계 조회(탐색)
    
     1) 무식한 방법
      : `logic()` 안에 `ac.getBean()`을 통해서 DL 직접 구현
     2) 스프링에서 제공하는 DL (권장)
      - ObjectFactory
       : getObject() 제공
      - ObjectProvider extends ObjectFactory
       : getObject() +a 제공
     3) 스프링에 의존하지 않는 DL (외부 라이브러리)
      - JSR-330 Provider
       : javax.inject.Provider (자바표준, 단순)

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,11 +2,15 @@ package hello.core.scope;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,7 +30,7 @@ public class SingletonWithPrototypeTest1 {
     }
 
     @Test
-    void singletonClientUsePrototype() {
+    void singletonClientUsePrototype1() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
 
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
@@ -35,15 +39,72 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int cnt2 = clientBean2.logic();
-        assertThat(cnt2).isEqualTo(2);
+        assertThat(cnt2).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUsePrototype2() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean_usdObjectProvider.class, PrototypeBean.class);
+
+        ClientBean_usdObjectProvider clientBean1 = ac.getBean(ClientBean_usdObjectProvider.class);
+        int cnt1 = clientBean1.logic();
+        assertThat(cnt1).isEqualTo(1);
+
+        ClientBean_usdObjectProvider clientBean2 = ac.getBean(ClientBean_usdObjectProvider.class);
+        int cnt2 = clientBean2.logic();
+        assertThat(cnt2).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUsePrototype3() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean_useProvider.class, PrototypeBean.class);
+
+        ClientBean_useProvider clientBean1 = ac.getBean(ClientBean_useProvider.class);
+        int cnt1 = clientBean1.logic();
+        assertThat(cnt1).isEqualTo(1);
+
+        ClientBean_useProvider clientBean2 = ac.getBean(ClientBean_useProvider.class);
+        int cnt2 = clientBean2.logic();
+        assertThat(cnt2).isEqualTo(1);
     }
 
     @Scope("singleton")
     @RequiredArgsConstructor
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
+//        private final PrototypeBean prototypeBean;
+        @Autowired
+        ApplicationContext ac;
 
         public int logic() {
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+            prototypeBean.addCnt();
+            return prototypeBean.getCnt();
+        }
+    }
+
+    @Scope("singleton")
+    @RequiredArgsConstructor
+    static class ClientBean_usdObjectProvider {
+
+        @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            prototypeBean.addCnt();
+            return prototypeBean.getCnt();
+        }
+    }
+
+    @Scope("singleton")
+    @RequiredArgsConstructor
+    static class ClientBean_useProvider {
+
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCnt();
             return prototypeBean.getCnt();
         }

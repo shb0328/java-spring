63ec3b6a4e59e3193fb21284773e6a94cce47a36
Hyebeen.So <hyebeen0328@naver.com>
2021-12-29 07:52:18 +0900

    빈 스코프 - 웹 스코프
    - 싱글톤 : 스프링 컨테이너의 생성 ~ 소멸까지 관리된다.
    - 프로토타입 : 스프링 컨테이너가 생성, 의존관계주입 후 관리하지 않는다. = 종료 메서드 호출X
    - 웹 스코프 : 스프링 컨테이너에 의해 종료시점까지 관리된다. = 종료 메서드 호출O
     - request
     - session
     - application
     - websocket
    
    * request 스코프 : HTTP 요청이 올때마다 별도의 빈 인스턴스 생성 및 관리
     > HTTP 요청을 받기전에 싱글톤 스코프 빈으로부터 의존관계주입 요청이 발생하는 문제? (Scope 'request' is not active for the current thread; ...)
     > Provide 사용으로 해결
    
    * 서비스 계층은 웹 기술에 종속되지 않는 것이 유지보수를 위해 좋다. 즉, 웹과 관련된 정보는 컨트롤러까지만 사용하자. (ex.requestURL)
    
    * UUID
     - java.util.UUID
    
    * 스프링부트 내장톰캣 사용
     - 'org.springframework.boot:spring-boot-starter-web'

---
+++ core/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,37 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+@Component
+@Scope(value = "request")
+public class MyLogger {
+
+    private String uuid; //HTTP 요청 당 생성되는 각각의 빈을 구분하기 위해서 사용 
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String msg) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + msg);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString(); // 유일한 UUID 발급
+        System.out.println("[" + uuid + "] request scope bean create : " + this);
+
+    }
+
+    //고객 요청이 서버에서 빠져나갈 때 스프링 컨테이너에 의해 close 호출되며 스코프 종료
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close : " + this);
+    }
+}

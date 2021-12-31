1f01391c5e318e8a8ad7a0da921416900542e0c6
Hyebeen.So <hyebeen0328@naver.com>
2021-12-31 22:46:58 +0900

    빈 스코프 - 스코프와 프록시
    * proxyMode
     - ScopedProxyMode.TARGET_CLASS
     - ScopedProxyMode.INTERFACES
    
    스프링은 일단, CGLIB 라이브러리로 가짜 프록시 객체를 만들어서 주입한다.
     .ex) LogDemoController 에서 MyLogger 필드에 의존성이 주입될 때
     .출력테스트) myLogger = class hello.core.common.MyLogger$$EnhancerBySpringCGLIB$$b25c30bf
     .심지어) ac.getBean 호출 시에도 프록시 객체가 조회된다.
     .그래서) 실제 객체가 필요할 때 (request 발생 후) 실제 Bean 이 초기화된다.
    
    가짜 프록시 객체는 내부에 실제 객체를 찾는 방법을 알고 있다.
     .위임 로직
     .싱글톤처럼 동작
    
    가짜 프록시 객체는 원본 클래스를 상속 받아서 만들어졌다. (다형성)
     .즉) 클라이언트는 실제 객체인지 프록시 객체인지 알 필요가 없다.
    
    * Provider vs. Proxy
    공통 핵심원리 : 실제 객체 조회를 필요한 시점까지 지연처리한다.
    
    * request scope 같은 특별한 scope 의 무분별한 사용은 유지보수를 어렵게 한다.

--- core/src/main/java/hello/core/common/MyLogger.java
+++ core/src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,7 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
 
     private String uuid; //HTTP 요청 당 생성되는 각각의 빈을 구분하기 위해서 사용 
